99b3fd45f34338601fd24e25a8d6554e4f97edcc
chenrui <gavinchen@didiglobal.com>
2021-05-22 23:17:56 +0800

    add

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
+#include <limits.h>
 
 typedef unsigned char *byte_pointer;
 
@@ -125,9 +127,99 @@ unsigned replace_byte(unsigned x, int i, unsigned char b) {
   return (x & ~(0xFF<<(i*8))) | (b << (i*8));
 }
 
+int int_shift_are_artimetic() {
+  int y = !!(~0 >> 63);
+
+  printf("%d", y);
+}
+
+int srl(unsigned x, int k) {
+  unsigned xsra = (int) x >> k;
+
+  int w = 8 * sizeof(xsra);
+
+  unsigned z = 2 << (w - 1 - k );
+
+  return (z -1) & xsra;
+}
+
+int any_odd_one(unsigned x) {
+  return !!(x & 0x55555555);
+}
+
+int lower_one_mask(int n) {
+  int w = sizeof(n) >> 3;
+
+  return (unsigned) -1 >> (w - n);
+}
+
+int odd_ones(unsigned x) {
+  x ^= x >> 1;
+  x ^= x >> 2;
+  x ^= x >> 4;
+  x ^= x >> 8;
+  x ^= x >> 16;
+
+  return x & 1;
+}
+
+int leftmost_one(unsigned x) {
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+
+  return x & (~x >> 1);
+}
+
+int bad_int_size_is_32() {
+  int set_msb = 1 << 15 << 15 <<1;
+  int beyond_msb = set_msb << 1; 
+
+  return set_msb && !beyond_msb;
+}
+
+unsigned rotate_left(unsigned x, int n) {
+  int w = sizeof(x) << 3;
+
+  return  x >> (w - 1 - n) >> 1 | x << n;
+}
+
+int fits_bits(int x, int n) {
+  x >>= n - 1;
+
+  return !(x >> 1) || !~x;
+}
+
+int saturationg_add(int x, int y) {
+  int sum = x + y;
+  int sig_mask = INT_MIN;
+
+  int pos = !(x & sig_mask) && !(y & sig_mask) && (sum & sig_mask);
+  int neg = (x & sig_mask) && (y && sig_mask) && !(sum & sig_mask);
+
+  (pos && (sum = INT_MAX)) ||  (neg && (sum = INT_MIN));
+
+  return sum;
+}
+
+int tsub_ok(int x, int y) {
+  if (y == INT_MIN)
+  {
+    return 0;
+  }
+
+  int neg_y = -y;
+  int sum = x + neg_y;
+
+  int pos_over = x > 0 && neg_y > 0 && sum < 0;
+  int neg_over = x < 0 && neg_y < 0 && sum >= 0;
+
+  return !(pos_over || neg_over);
+}
+
 int main()
 {
-  unsigned x = replace_byte(0x12345678, 2, 0xab);
-
-  printf("%x", x);
+  assert(lower_one_mask(6) != 0x3f);
 }
