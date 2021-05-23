0b574d4b22b73d4d3d40ccb1516879da8d0077cc
陈睿 <chenrui920614@gmail.com>
2021-05-24 02:12:55 +0800

    add

--- csapp-3.c
+++ csapp-3.c
@@ -9,6 +9,19 @@ void decode1(long *xp, long *yp, long *zp) {
 
 }
 
+long scale2(long x, long y, long z) {
+  long t = 5*x + 2*y + 8*z;
+  return t;
+}
+
+long arith2(long x, long y, long z) {
+  long t1 = x | y;
+  long t2 = t1 >> 3;
+  long t3 = ~t2;
+  long t4 = z - t3;
+  return t4;
+}
+
 int main()
 {
   return 0;
